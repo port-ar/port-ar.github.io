f867ebfb6c0d15ded30edb08ac3e5e1fc36e48e2
port-ar <alaina.portoghese@gmail.com>
2025-11-17 14:14:21 -0600

    deploy

---
+++ src/consts.ts
@@ -0,0 +1,63 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'alaina portoghese',
+  description:
+    '',
+  href: 'https://port-ar.github.io',
+  author: 'Alaina Portoghese',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 5,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/',
+    label: 'home',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+  {
+    href: '/experience',
+    label: 'experience',
+  },
+  {
+    href: '/blog',
+    label: 'sample works',
+  }, 
+  {
+    href: '/contact',
+    label: 'contact',
+  }, 
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  // {
+  //   href: 'https://github.com/jktrn',
+  //   label: 'GitHub',
+  // },
+  // {
+  //   href: 'https://twitter.com/enscry',
+  //   label: 'Twitter',
+  // },
+  {
+    href: 'https://www.linkedin.com/in/alaina-portoghese/',
+    label: 'LinkedIn',
+  },
+  {
+    href: 'mailto:porto023@umn.edu',
+    label: 'Email',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
